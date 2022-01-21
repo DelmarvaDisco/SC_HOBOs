d5b137d69a03f2245446937d87e492f8b0810597
James Maze <jtmaze@vt.edu>
2022-01-21 18:12:36 -0500

    Friyay!

--- Workspace.R
+++ Workspace.R
@@ -5,6 +5,9 @@
 # Purpose: Plot and analyze Jackson Lane SC data
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+#   Notes:
+#     - For the Fall 2020 files, I launched sensors only to measure full range. 
+#       Unfortunately low range is more appropriate for our sites. 
 
 # 1. Libraries and workspace ----------------------------------------------
 
@@ -14,15 +17,40 @@ library(xts)
 library(dygraphs)
 library(purrr)
 library(lubridate)
+library(stringr)
 library(tidyverse)
 library(stringr)
 
-data_dir <- "\data"
+data_dir <- "data/JL/"
 
 
 # 2. Read in the files ----------------------------------------------------
 
-files <- list.files(path = paste0(data_dir), full.names = TRUE)
+files <- list.files(paste0(data_dir), full.names = TRUE, pattern = ".csv")
+
+#Download this mess
+data <- files %>% 
+  map(download_hobo) 
+
+#Mush the tibbles together
+data_comb <- data %>% 
+  reduce(bind_rows)
+
+#Rename cols to something manageable
+colnames(data_comb) <- c("record_num", 
+                         "Timestamp", 
+                         "Full_range", 
+                         "Temp_C", 
+                         "file", 
+                         "Site_ID", 
+                         "serial_number",
+                         "time_zone",
+                         "Low_range")
+
+
+# 3. Reformat the data ---------------------------------------------------------------------
+
+
 
 
 
---
+++ download_hobo.R
@@ -0,0 +1,37 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Name: Hobo SC download
+# Coder: James Maze with help from Nate Jone's waterLevel
+# Date: 21 Jan 2021
+# Purpose: Download lots of SC files
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+download_hobo <- function(files){
+  
+  temp <- read_csv(paste0(files),
+                   skip = 1,
+                   col_names = TRUE) %>% 
+    as.tibble() %>% 
+    # Only select important cols, forget that couple attached stuff
+    select(!contains(c("Coupler", "Host", "End of", "Stopped"))) %>% 
+    # Add a column for the file name
+    mutate(file = str_sub(files, 9)) %>% 
+    # Get the site from the file name
+    mutate(Site_ID = str_sub(file, 1, 5))
+  
+  #"Borrowed" this code from Nate Jones
+  serial_number <- colnames(temp)[grep("LGR",colnames(temp))][1]
+  time_zone <- colnames(temp)[grep("GMT",colnames(temp))] 
+  
+  #This is an ugly fix, but I couldn't just name columns bc files had different column numbers
+  cols <- colnames(temp)
+  colzzz <- str_trunc(cols, width = 7, side = "right", ellipsis = "_")
+  colnames(temp) <- colzzz
+
+  #Add columns to denote sn and tz
+  temp <- temp %>% 
+    add_column(serial_number) %>% 
+    add_column(time_zone)
+  
+  
+}
+    
